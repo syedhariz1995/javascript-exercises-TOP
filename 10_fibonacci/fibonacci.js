4da4e5bc459e4b51468f7131dac185d49742d14f
syedhariz1995 <syedhariz1995@gmail.com>
2023-10-13 11:25:22 +0800

    done with fibonachi

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0 ) return "OOPS"
+    if (num === 0) return 0
+  
+   let first = 1;
+   let second = 0;
+  
+   for (i = 2; i <= num; i++){
+    let current = first + second
+    second = first
+    first = current
+   }
+   return first
+  }
+  
+  
+  console.log(fibonacci(4))
+  console.log(fibonacci(6))
+  console.log(fibonacci(10))
+  console.log(fibonacci(15))
+  console.log(fibonacci(25))
+  console.log(fibonacci(-25))
+  console.log(fibonacci("1"))
+  console.log(fibonacci("2"))
+  console.log(fibonacci("8"))
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
